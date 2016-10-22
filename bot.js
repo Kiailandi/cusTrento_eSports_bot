fabd519cb24d5b90ebc50346b0e38ab71ed9e272
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2016-10-22 23:59:07 +0200

    Try to fix production mode

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var bot;
 
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot.setWebHook(process.env.HEROKU_URL + token);
 }
 else {
   bot = new Bot(token, { polling: true });
